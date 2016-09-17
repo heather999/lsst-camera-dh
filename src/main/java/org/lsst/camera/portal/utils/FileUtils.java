702e509b7ab0b0c9288e9b6c33924d2036624958
Tony Johnson <tonyj@stanford.edu>
2016-09-16 18:06:20 -0700

    In progress changes for implementing LSSTTD-703

---
+++ src/main/java/org/lsst/camera/portal/utils/FileUtils.java
@@ -0,0 +1,56 @@
+package org.lsst.camera.portal.utils;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.SortedMap;
+import javax.servlet.jsp.jstl.sql.Result;
+
+/**
+ * File handling utilities for the data portal
+ * @author tonyj
+ */
+public class FileUtils {
+    /**
+     * Extract a List from a single column of a result set
+     * @param query The result of an sql query
+     * @param column The column to extract
+     * @return The extracted list.
+     */
+    public static List getColumnFromResult(Result query, String column) {
+        List result = new ArrayList<>();
+        for (SortedMap row : query.getRows()) {
+            result.add(row.get(column));
+        }
+        return result;
+    }
+    /**
+     * Find the common root of a list of files
+     * @param files A list of files
+     * @return The common root
+     */
+    public static String commonRoot(List files) {
+        String result = null;
+        for (Object path : files) {
+            String file = path.toString();
+            if (result == null) result = file;
+            else if (!file.startsWith(result)) {
+               List<String> split = Arrays.asList(file.split("/"));
+               for (int i=split.size(); i>0; i--) {
+                   String join = String.join("/",split.subList(0, i));
+                   if (result.startsWith(join)) {
+                       result = join;
+                       break;
+                   }
+               } 
+            } 
+        }
+        return result == null ? "" : result;
+    }
+    
+    public static String relativize(String root, String file) {
+        return file.startsWith(root) ? file.substring(root.length()) : file;
+    }
+}
